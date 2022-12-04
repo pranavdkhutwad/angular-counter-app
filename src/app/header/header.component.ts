904b255a22ca0016cb860a9f4597ab85e0c126b6
Ganesh Khutwad <pranavdkhutwad@gmail.com>
2022-12-04 11:58:59 +0530

    adding files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+// Decorator
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cnt-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
